1478150443e90914627fd0b64917186a901c6f2f
quantdevlike <hamedabbaszadeh91@gmail.com>
2024-03-06 12:00:38 +0100

    create user table and authenticate user login

--- models.py
+++ models.py
@@ -1,5 +1,18 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String, unique=False)
+    last_name = Column(String, unique=False)
+    hashed_passwd = Column(String, unique=False)
+    is_active = Column(Boolean, default=True)
+    role = Column(String, unique=False)
 
 
 class Todos(Base):
@@ -10,4 +23,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-    
+    owner_id = Column(Integer, ForeignKey("users.id"))
