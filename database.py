dbac403da37b7aa621408e859eeca8d93d883d90
quantdevlike <hamedabbaszadeh91@gmail.com>
2024-03-07 15:18:12 +0100

    unit tests for main and todos

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:******@localhost/TodoApplicationDatabase'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
--- main.py
+++ main.py
@@ -12,6 +12,12 @@ models.Base.metadata.create_all(bind=engine)
 
 logging.getLogger('passlib').setLevel(logging.ERROR)
 
+
+@app.get("/healthy")
+def health_check():
+    return {'status': 'Healthy'}
+
+
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
