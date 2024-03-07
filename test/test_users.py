8cec439635785c3d72c5bcaf3492bb91f87dafcb
quantdevlike <hamedabbaszadeh91@gmail.com>
2024-03-07 17:06:39 +0100

    unit test for auth router

--- test/test_users.py
+++ test/test_users.py
@@ -2,7 +2,6 @@ from starlette import status
 
 from .utils import *
 from routers.users import get_current_user, get_db
-from models import Users
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
